d78a1fe3c1c7ed334c3d36f82369d061795d0f90
Robinahood1311 <59414122+Robinahood1311@users.noreply.github.com>
2020-03-06 11:59:28 +0800

    heroku initial

--- instagram_api/blueprints/users/views.py
+++ instagram_api/blueprints/users/views.py
@@ -1,9 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from models.user import User
+from playhouse.shortcuts import model_to_dict
 
 users_api_blueprint = Blueprint('users_api',
-                             __name__,
-                             template_folder='templates')
+                                __name__,
+                                template_folder='templates')
+
 
 @users_api_blueprint.route('/', methods=['GET'])
 def index():
-    return "USERS API"
+    users = User.select()
+    user_data = []
+    # model_to_dict converts user into DICTIONARY
+    for user in users:
+        model_to_dict(user)
+        del user['password']
+        # to delete password in dictionary
+        user_data.append(user)
+
+    return jsonify(user_data), 200
+    # status code 200: this request is successful
+    # returns json with all the user information
